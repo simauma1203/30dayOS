9a72c5f61fc81067a7956d52c48cc3c080d9795c
simauma1203 <20992019+simauma1203@users.noreply.github.com>
2018-06-14 21:57:28 +0900

    5day-F

--- bootpack.c
+++ bootpack.c
@@ -10,6 +10,10 @@ void set_palette(int start,int end,unsigned char *rgb);
 void boxfill8(unsigned char *vram,int xsize,unsigned char c,
                 int x0,int y0,int x1,int y1);
 void init_screen(char *vram,int x,int y);
+void putfont8(char *vram,int xsize,int x,int y,char c,char *font);
+void putfonts8_asc(char *vram,int xsize,int x,int y,char c,unsigned char *s);
+
+
 
 //色コード定義
 #define COL8_000000		0
@@ -37,18 +41,15 @@ struct BOOTINFO{
 
 void HariMain(void){
 
-    char *vram;
-    int xsize,ysize;
-    struct BOOTINFO *binfo;
+    struct BOOTINFO *binfo=(struct BOOTINFO *)0x0ff0;//番地変数　0xff0から12BはBOOTINFO
 
     init_palette();
-    binfo=(struct BOOTINFO *)0x0ff0;//番地変数　0xff0から12BはBOOTINFO
-    xsize=(*binfo).scrnx;
-    ysize=(*binfo).scrny;
-    vram=(*binfo).vram;
-
-    init_screen(vram,xsize,ysize);
+    init_screen(binfo->vram,binfo->scrnx,binfo->scrny);
     
+    putfonts8_asc(binfo->vram, binfo->scrnx,  8,  8, COL8_FFFFFF, "ABC 123");
+	putfonts8_asc(binfo->vram, binfo->scrnx, 31, 31, COL8_000000, "dekita-!");
+	putfonts8_asc(binfo->vram, binfo->scrnx, 30, 30, COL8_FFFFFF, "dekita-!");
+
     for(;;){
         io_hlt();
     }
@@ -121,6 +122,36 @@ void init_screen(char *vram,int x,int y){
     boxfill8(vram,x,COL8_848484,x-47,y-23,x-47,y-4);
     boxfill8(vram,x,COL8_FFFFFF,x-47,y-3,x-4,y-3);
     boxfill8(vram,x,COL8_FFFFFF,x-3,y-24,x-3,y-3);
+
+    return;
+}
+
+void putfont8(char *vram,int xsize,int x,int y,char c,char *font){
+    int i;
+    char *p,d;//data
+    for(i=0;i<16;i++){
+        p=vram+(y+i)*xsize+x;
+        d=font[i];
+        if((d & 0x80)!=0){p[0]=c;}
+        if((d & 0x40)!=0){p[1]=c;}
+        if((d & 0x20)!=0){p[2]=c;}
+        if((d & 0x10)!=0){p[3]=c;}
+        if((d & 0x08)!=0){p[4]=c;}
+        if((d & 0x04)!=0){p[5]=c;}
+        if((d & 0x02)!=0){p[6]=c;}
+        if((d & 0x01)!=0){p[7]=c;}
+
+    }
+    return;
+}
+
+void putfonts8_asc(char *vram, int xsize, int x, int y, char c, unsigned char *s){
+
+    extern char hankaku[4096];
+    for(;*s!=0x00;s++){
+        putfont8(vram,xsize,x,y,c,hankaku+ *s *16);
+        x+=8;
+    }
 
     return;
 }
